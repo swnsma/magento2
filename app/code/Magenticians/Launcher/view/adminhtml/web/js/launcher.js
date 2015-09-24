0a105a0ed97c17c700b48323da20af3d6c7f3757
Aleksandr Kravchuk <al.kravchuk@ism-ukraine.com>
2015-09-24 10:09:43 +0300

    Magenticians Launcher. Add module.

---
+++ app/code/Magenticians/Launcher/view/adminhtml/web/js/launcher.js
@@ -0,0 +1,47 @@
+/**
+ * Created by al.kravchuk on 23.09.15.
+ */
+setTimeout(function(){require([
+    'jquery',
+    'jquery/ui'
+    ], function($) {
+        $(function() {
+
+
+            $('#magenticians-launcher-link').click(function () {
+                $('#magenticians-launcher-dialog').dialog({
+                    //autoOpen: true,
+                    modal: true,
+                    open: function() {
+                        $(this).closest('.ui-dialog').addClass('ui-dialog-active');
+
+                        //var topMargin = $(this).closest('.ui-dialog').children('.ui-dialog-titlebar').outerHeight() + 10;
+                        //$(this).closest('.ui-dialog').css('margin-top', topMargin);
+                    }
+                });
+            });
+
+            $('#magenticians-launcher-input').autocomplete({
+                source: launcher_items,
+
+                // On selecting an entry, we point the document to the location attached to it
+                select: function (event, ui) {
+                    event.preventDefault();
+                    document.location = $(this).attr('data-target');
+                },
+
+                // When an entry receives focus we display the label in the input and store the URL in "data-target"
+                focus: function (event, ui) {
+                    event.preventDefault();
+                    $(this).val(ui.item.label);
+                    $(this).attr('data-target', ui.item.value);
+                },
+
+                // When the autocomplete widget is initialized, we quickly remove the accessible helper as we don't need it
+                create: function (event) {
+                    $(this).next('.ui-helper-hidden-accessible').remove();
+                }
+            });
+        });
+});
+}, 1000);
--- app/etc/config.php
+++ app/etc/config.php
@@ -3,6 +3,7 @@ return array (
   'modules' => 
   array (
     'Ashsmith_HelloWorld' => 1,
+    'Magenticians_Launcher' => 1,
     'Magento_Store' => 1,
     'Magento_AdvancedPricingImportExport' => 1,
     'Magento_Directory' => 1,
